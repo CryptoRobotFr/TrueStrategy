dff7d02022588aa556a53182d38d09924922484d
Crypto Robot <88462650+CryptoRobotFr@users.noreply.github.com>
2021-08-12 13:42:35 +0200

    Update #1FTX.py

--- Cross EMA + stoch RSI#1/#1FTX.py
+++ Cross EMA + stoch RSI#1/#1FTX.py
@@ -30,6 +30,7 @@ df['STOCH_RSI']=ta.momentum.stochrsi(df['close'])
 
 def getBalance(myclient, coin):
     jsonBalance = myclient.get_balances()
+    if jsonBalance == [] : return 0
     pandaBalance = pd.DataFrame(jsonBalance)
     if pandaBalance.loc[pandaBalance['coin'] == coin].empty : return 0
     else : return float(pandaBalance.loc[pandaBalance['coin'] == coin]['free'])
