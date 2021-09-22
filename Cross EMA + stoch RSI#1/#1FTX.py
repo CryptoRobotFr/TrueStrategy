819506cd4853cf5f603941fcdb695d945c6b7cac
Crypto Robot <88462650+CryptoRobotFr@users.noreply.github.com>
2021-09-22 17:21:15 +0200

    Update #1FTX.py

--- Cross EMA + stoch RSI#1/#1FTX.py
+++ Cross EMA + stoch RSI#1/#1FTX.py
@@ -30,10 +30,14 @@ df['STOCH_RSI']=ta.momentum.stochrsi(df['close'])
 
 def getBalance(myclient, coin):
     jsonBalance = myclient.get_balances()
-    if jsonBalance == [] : return 0
+    if jsonBalance == []: 
+        return 0
     pandaBalance = pd.DataFrame(jsonBalance)
-    if pandaBalance.loc[pandaBalance['coin'] == coin].empty : return 0
-    else : return float(pandaBalance.loc[pandaBalance['coin'] == coin]['free'])
+    print(pandaBalance)
+    if pandaBalance.loc[pandaBalance['coin'] == coin].empty: 
+        return 0
+    else: 
+        return float(pandaBalance.loc[pandaBalance['coin'] == coin]['total'])
 
 def truncate(n, decimals=0):
     r = floor(float(n)*10**decimals)/10**decimals
